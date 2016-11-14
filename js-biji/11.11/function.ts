687a4cc4386bb62c95e266410782ec15ccfbf78f
gujiajia <947452831@qq.com>
2016-11-14 09:08:10 +0800

    11.14

---
+++ js-biji/11.11/function.ts
@@ -0,0 +1,47 @@
+// var func2 = function () {
+// console.log('func2');
+// }
+// console.log(func2);
+// func2();
+
+// var add = function (a, b) {
+// return a + b;
+// };
+// function add() {
+
+// }
+
+/*function funcA(func) {
+    console.log(func);
+    func();
+}
+function funcB() {
+    console.log('我是函数B');
+}
+funcA(funcB);
+*/
+/*function action() {
+    return function () {
+        console.log('im action');
+    }
+}
+var res = action();
+console.log(res);
+res();
+*/
+/*var add = (a, b) => a + b;
+var addResult = add(2, 2);
+console.log(addResult);
+var squre = a => a * a;
+var squreResult = squre(2);
+console.log(squreResult);
+*/
+var arr = [];
+arr.push('xiaozhang');
+arr.push('xiaoming');
+console.log(arr);
+console.log(arr.length);
+// var lastPeoson = arr.pop();
+// console.log(lastPeoson);
+arr.unshift('xiaobai');
+console.log(arr);

ec9ef59717290f857b55666029c5ba87e460302f
gujiajia <947452831@qq.com>
2016-11-17 17:22:27 +0800

    11.17

--- js-biji/11.17/function.js
+++ js-biji/11.17/function.js
@@ -1,14 +1,19 @@
-var price = parseFloat(prompt('请输入商品单价'));
-var quantity = parseInt(prompt('请输入购买数量'));
-var received = parseFloat(prompt('请输入收款金额'));
+ var price = Number(prompt('请输入商品的单价'));
+ var quantity = Number(prompt('请输入购买的数量'));
+ var received = Number(prompt('请输入收款金额'));
 
-function amount(a, b) {
-    if ((a * b) >= 500) {
-        return (a * b) * 0.8;
-    }
-    return a * b;
-}
-alert(`
-总金额:${amount(price,quantity)}
-找零:${received - amount(price,quantity)}
+ if (isNaN(price || quantity || received)) {
+     alert('请输入数字');
+ }
+
+ function amount(a, b) {
+     if ((a * b) >= 500) {
+         return (a * b) * 0.8;
+     }
+     return a * b;
+ }
+
+ alert(`
+总金额:${amount(price,quantity).toFixed(2)}
+找零:${received - amount(price,quantity).toFixed(2)}
 `);
